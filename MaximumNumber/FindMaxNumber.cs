59a3df36b1d970298802b171f4bc5405c566c006
VSRUJANA <srujana.valavala@gmail.com>
2020-10-20 17:14:43 +0530

    [srujana] Add. Method to get maximum of 3 integers added

---
+++ MaximumNumber/FindMaxNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumNumber
+{
+    public class FindMaxNumber
+    {
+        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            try
+            {
+                if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                {
+                    return firstValue;
+                }
+                if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                {
+                    return secondValue;
+                }
+                if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+                {
+                    return thirdValue;
+                }
+                throw new Exception("FirstNumber,SecondNumber and ThirdNumber are same");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught! " + e.Message);
+                System.Environment.Exit(0);
+                return 0;
+            }
+            
+        }
+
+    }
+}
--- MaximumNumber/Program.cs
+++ MaximumNumber/Program.cs
@@ -7,6 +7,12 @@ namespace MaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to find Maximum number Program! ");
+            Console.WriteLine("Enter 3 integers");
+            int i1 = int.Parse(Console.ReadLine());
+            int i2 = int.Parse(Console.ReadLine());
+            int i3 = int.Parse(Console.ReadLine());
+            int output = FindMaxNumber.MaximumIntegerNumber(i1,i2,i3);
+            Console.WriteLine("Maximum Integer among "+i1+","+i2+ "," + i3+ " : "+output);
         }
     }
 }
