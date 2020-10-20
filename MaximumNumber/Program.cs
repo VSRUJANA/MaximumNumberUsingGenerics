955f2a769eb2801775740afcf62e36e666ba5a13
VSRUJANA <srujana.valavala@gmail.com>
2020-10-20 20:27:33 +0530

    [srujana] Refactor. Instead of 3 methods, a generic method to find maximum is added

--- MaximumNumber/Program.cs
+++ MaximumNumber/Program.cs
@@ -6,7 +6,24 @@ namespace MaximumNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to find Maximum number Program! ");
+            Console.WriteLine("Welcome to find Maximum number Program!");
+            Console.WriteLine("\nEnter 3 integer Numbers");
+            int i1 = int.Parse(Console.ReadLine());
+            int i2 = int.Parse(Console.ReadLine());
+            int i3 = int.Parse(Console.ReadLine());
+            FindMaxNumber.MaximumCheck<int>(i1,i2,i3);
+            Console.WriteLine("\nEnter 3 float Numbers");
+            float f1 = float.Parse(Console.ReadLine());
+            float f2 = float.Parse(Console.ReadLine());
+            float f3 = float.Parse(Console.ReadLine());
+            FindMaxNumber.MaximumCheck<float>(f1,f2,f3);
+            Console.WriteLine("\nEnter 3 Strings");
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            string s3 = Console.ReadLine();
+            FindMaxNumber.MaximumCheck<string>(s1,s2,s3);
+            
+            /*
             Console.WriteLine("Enter 3 integer Numbers");
             int i1 = int.Parse(Console.ReadLine());
             int i2 = int.Parse(Console.ReadLine());
@@ -27,6 +44,7 @@ namespace MaximumNumber
             string s3 = Console.ReadLine();
             string output3 = FindMaxNumber.MaximumString(s1, s2, s3);
             Console.WriteLine("Maximum String among " + s1 + ", " + s2 + ", " + s3 + " : " + output3);
+            */
         }
     }
 }
